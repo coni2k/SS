a655142195e7fd585def40ce22f2f6d51b4b4297
Serkan Holat <serkanholat@hotmail.com>
2012-05-22 09:52:49 +0200

    RunIISExpress.bat update
    This file was updated earlier but somehow that modification was not
    there?
    Now it's updated again.

--- RunIISExpress.bat
+++ RunIISExpress.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\IIS Express\iisexpress.exe" /path:"D:\Development\Projects\SS\SudokuSolver.WebApp" /port:56105
+"%ProgramFiles%\IIS Express\iisexpress.exe" /path:"%CD%\SudokuSolver.WebApp" /port:56105
+pause
